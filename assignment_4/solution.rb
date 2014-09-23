8bd49ba5911034f3fe2dc9761d236a710c7da5eb
Ahmad Hutasuhut <ahutasuh@gmail.com>
2014-09-23 13:24:29 -0400

    Created solution.rb to show different results than assignment_4.rb

---
+++ assignment_4/solution.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/ruby
+
+print "1. "
+puts 5 * 2
+
+print "2. "
+puts 5 > 4
+
+print "3. "
+puts (6 * 2) > 10
+
+print "4. "
+puts 2 == 3
+
+print "5. "
+puts (1 > 0) && (2 <= 3)
+
+print "6. "
+puts 2 != 1
+
+print "7. "
+puts (14 <= 14) || (3 >= 3)
+
+print "8. "
+puts 10 % 2 == 0
+
+print "9. "
+puts 10 ** 3
+
+print "10. "
+puts 1000 / 250

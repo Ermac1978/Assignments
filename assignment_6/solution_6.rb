4a1204e421121076aaaa61e61de14c31f6aa3544
Ahmad Hutasuhut <ahutasuh@gmail.com>
2014-09-24 10:30:20 -0400

    Assignment #6, Beginning with Variables, Arrays, Hashes, Literals, Symbols, Constants

---
+++ assignment_6/solution_6.rb
@@ -0,0 +1,44 @@
+# Create a String variable named 'name'
+name = "Ahmad"
+# Create a Date variable named "a_date"
+require 'date'
+a_date = Date.new(1978,06,11)
+# Create an integer variable named "age"
+age = 36
+# Create an array of names named 'names'
+names = ["Ahmad", "Nurdin", "Hutasuhut"]
+# Create another array, using %w, named 'more_names'
+more_names = %w(Ahmad Nurdin Hutasuhut)
+# Create a Hash of names with ages named 'names_and_ages'
+names_and_ages = {names => age}
+# Keep this part as a way to test your code
+puts name.class
+puts a_date.class
+puts age.class
+puts names.class
+puts more_names.class
+puts names_and_ages.class
+
+puts name.is_a? String
+puts a_date.is_a? Date
+puts age.is_a? Fixnum
+puts names.is_a? Array
+puts more_names.is_a? Array
+puts names_and_ages.is_a? Hash
+
+# Create a String literal and output it's class (one line)
+puts "This is Ahmad's string".class
+# Create a Fixnum literal and output it's class (one line)
+puts 36.class
+# Create an Array literal and output it's class (one line)
+puts ["Ahmad", "Nurdin", "Hutasuhut"].class
+# Create a Hash literal and output it's class (one line). Note to keep from confusing Ruby you'll have to wrap what you are outputting in parenthesis
+puts hsh = {names => age}.class
+# Create a symbol and output it's class
+puts :"My name is Ahmad".class
+# Create a String constant with any name you choose
+NAME = "Ahmad"
+# Change the value of that constant
+NAME = "Andy"
+# Output the constant's value to the screen
+puts NAME

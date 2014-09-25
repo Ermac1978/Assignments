ed1c804b604ce31acb8eef3449570beb52c8da63
Ahmad Hutasuhut <ahutasuh@gmail.com>
2014-09-24 23:13:47 -0400

    Assignment #7, happy birthday script ver.2

---
+++ assignment_8/solution_8.rb
@@ -0,0 +1,13 @@
+require 'date'
+
+if ARGV.size == 0
+   print "What is your name? "
+   name = $stdin.gets.chomp
+   print "When is your birthday (enter in order YYYY-MM-DD)? "
+   bday = Date.parse($stdin.gets.chomp)
+   puts "Happy Birthday #{name.capitalize} (#{bday.strftime("%A, %B %d, %Y")})!"
+else
+  print "When is your birthday (enter in order YYYY-MM-DD)? "
+  bday = Date.parse($stdin.gets.chomp)
+  puts "Happy Birthday #{ARGV[0].capitalize} (#{bday.strftime("%A, %B %d, %Y")})!"
+end

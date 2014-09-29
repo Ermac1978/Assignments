f7e65408971cba88006abf3d127373279e057d35
Ahmad Hutasuhut <ahutasuh@gmail.com>
2014-09-29 16:15:43 -0400

    Assignment #14, solution_14.rb, unfinished assignment

---
+++ assignment_14/solution_14.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/ruby
+# Note for this exercise, follow these simplified pig latin rules
+# If the first letter is a vowel, add "way" to the end
+# If the first letter is a consonant, move it to the end and add "ay"
+
+def starts_with_vowel(text)
+  if text [0] = /[aeiou]/
+    return true
+  else
+    return false
+end
+puts starts_with_vowel.class
+
+def pigatize(text)
+  if starts_with_vowel(text)
+  else
+  end
+  return pigatized_text
+end
+
+
+
+loop do
+  puts "Please enter a word and I will translate to pig latin"
+  text = gets.chomp
+  break if text.length == 0
+  puts pigatize(text)
+end

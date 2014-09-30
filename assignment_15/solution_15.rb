5dd0f6d7cabdc9adbf99280a14c39f155072a580
Ahmad Hutasuhut <ahutasuh@gmail.com>
2014-09-30 10:27:23 -0400

    Assignment#15, solution_15.rb, loading files into Ruby

---
+++ assignment_15/solution_15.rb
@@ -0,0 +1,13 @@
+# Assignment 15 - in class
+# Modify assignment 12 to read the questions from a file instead of the hardcoded array
+# Load them into an array so the rest of the program can remain the same
+# No looping
+
+
+file = File.new('sample_questions.txt')
+questions = file.readlines
+
+questions.each do |question|
+  print question
+  answer = gets.chomp
+end

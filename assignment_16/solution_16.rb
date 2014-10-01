98cafbc38a79535045c74975bbb82bdeffef0351
Ahmad Hutasuhut <ahutasuh@gmail.com>
2014-10-01 09:56:11 -0400

    Asignment#16, solution_16.rb, pulling from the internet text and writing to movie_script_file.txt

--- assignment_16/solution_16.rb
+++ assignment_16/solution_16.rb
@@ -2,22 +2,20 @@
 # Pick a screenplay from this list, http://www.simplyscripts.com/a.html, in text
 # Read in the screenplay and write it to a local file
 
-
-require 'net/http'
-
-
+=begin
+Alternate Methods:
 # uri = URI('http://www.dailyscript.com/scripts/Aliens_James_Cameron_May_28_1985_first_draft.html')
+# script_text = Net::HTTP.get(uri)
 
 # OR
 
 # Net::HTTP.get(URI('http://www.dailyscript.com/scripts/Aliens_James_Cameron_May_28_1985_first_draft.html'))
+# script_text = Net::HTTP.get(uri)
+=end
 
-# OR
+require 'net/http'
 
 script_text = Net::HTTP.get(URI('http://www.dailyscript.com/scripts/Aliens_James_Cameron_May_28_1985_first_draft.html'))
 
-# script_text = Net::HTTP.get(uri)
-
 file = File.new("movie_script_file.txt", 'a')
-
 file.write(script_text)

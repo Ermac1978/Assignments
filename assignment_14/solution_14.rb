86568ca944720a48bf46d42fd18abdc6b330e1cf
Ahmad Hutasuhut <ahutasuh@gmail.com>
2014-10-01 23:21:55 -0400

    Assignment #14, solution_14.rb, working with defining methods

--- assignment_14/solution_14.rb
+++ assignment_14/solution_14.rb
@@ -3,23 +3,18 @@
 # If the first letter is a vowel, add "way" to the end
 # If the first letter is a consonant, move it to the end and add "ay"
 
-def starts_with_vowel(text)
-  if text [0] = /[aeiou]/i
-    return true
+def pigatize(passed_text)
+  if starts_with_vowel(passed_text[0])
+    pigatized_text = passed_text + "way"
   else
-    return false
-  end
-end
-
-
-def pigatize(text)
-  if starts_with_vowel(text)
-    pigatized_text = text + "way"
-  else
+    pigatized_text = passed_text[1..-1] + passed_text[0].downcase + "ay"
   end
   return pigatized_text
 end
 
+def starts_with_vowel(first_letter)
+  return ["a", "e", "i", "o", "u"].include?(first_letter)
+end
 
 loop do
   puts "Please enter a word and I will translate to pig latin"

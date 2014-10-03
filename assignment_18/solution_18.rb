1bc7f32e0c3d979a0018e08e93b4e7005e510e81
Ahmad Hutasuhut <ahutasuh@gmail.com>
2014-10-02 22:22:17 -0400

    Assignment #18, solution_18.rb, Movie Querying script

--- assignment_18/solution_18.rb
+++ assignment_18/solution_18.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Assignment 18: Movie Querying script
 # Use IMDB's movie querying script to create a command line app which prompts the user for a movie title
 # Using the entered movie title, search via the API for the movie's plot (http://www.myapifilms.com/)
@@ -10,34 +9,11 @@
 require 'net/http'
 require 'json'
 
-
 print "Please enter a movie title: "
 movie_title = gets.chomp
-
 
 uri = URI('http://www.myapifilms.com/imdb' + "?title=#{URI.escape(movie_title)}")
 movie_plot = Net::HTTP.get(uri)
 plot = JSON.parse(movie_plot)
 
 puts plot[0]["plot"]
-=======
-=begin
-# Assignment 18: Movie Querying script
-# Use IMDB's movie querying script to create a command line app which prompts the user for a movie title
-* Using the entered movie title, search via the API for the movie's plot
-* Print the plot to the screen
-=end
-require 'net/http'
-require 'uri'
-
-print "Name a movie?\n"
-movie = gets.chomp
-
-parms =  movie
-
-uri = URI('http://www.myapifilms.com/imdb?title=' + movie)
-
-response = Net::HTTP.get_response(uri)
-
-print response.body
->>>>>>> upstream/master

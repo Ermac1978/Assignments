2fa8c6bb308388585bea6386a9995507d431c8da
Ahmad Hutasuhut <ahutasuh@gmail.com>
2014-10-06 19:52:24 -0400

    Assignment #21, solution_21, ver.2, creating a Person class with methods

--- assignment_21/solution_21.rb
+++ assignment_21/solution_21.rb
@@ -21,9 +21,19 @@ require 'date'
 
 class Person
 
+  @@user_count = 0
+
   attr_reader :first_name
   attr_reader :last_name
   attr_accessor :bdate
+
+  def initialize
+    @@user_count += 1
+  end
+
+  def self.number_of_users
+    @@user_count
+  end
 
   def name(user_name)
     name_array = user_name.split
@@ -34,15 +44,22 @@ class Person
   def age
     print "When is your birthday (enter in order YYYY-MM-DD)? "
     @bday = Date.parse($stdin.gets.chomp)
+    calculateAge
+  end
+
+  private
+  def calculateAge
     user_age = (Date.today - @bday).to_i / 365
   end
+
 end
 
-
-user = Person.new
-print "What is your name? "
-user_name = gets.chomp
-
-user.name(user_name)
-user_age = user.age
-puts "Your name is #{user_name} and your age is #{user_age}."
+loop do
+  user = Person.new
+  print "What is your name? (enter \"q\" to quit) "
+  user_name = gets.chomp
+  break if user_name == "q"
+  user.name(user_name)
+  user_age = user.age
+  puts "Your name is #{user_name}. You are user number #{Person.number_of_users} and your age is #{user_age}."
+end

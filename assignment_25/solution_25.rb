ba806861176b4efc963df7fbed8b3a93982ddf48
Ahmad Hutasuhut <ahutasuh@gmail.com>
2014-10-07 10:55:30 -0400

    Assignment #25, solution_25.rb

--- assignment_25/solution_25.rb
+++ assignment_25/solution_25.rb
@@ -1,3 +1,7 @@
+# Assignment 25 - in class
+# See the following gist. Create classes that make sense for better encapsulating attributes and methods
+# https://gist.github.com/brianburridge/3eabb00826af747de2a2
+
 class Employee
 
    attr_accessor = :employee_name
